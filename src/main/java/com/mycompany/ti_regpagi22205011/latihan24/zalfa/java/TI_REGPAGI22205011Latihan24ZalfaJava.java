15c06f1366115dfe56eb2ef71bde0b284b285c49
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-10-26 12:52:49 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/ti_regpagi22205011/latihan24/zalfa/java/TI_REGPAGI22205011Latihan24ZalfaJava.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ti_regpagi22205011.latihan24.zalfa.java;
+import java.util.Scanner;
+/**
+ *
+ * @author User
+ */
+public class TI_REGPAGI22205011Latihan24ZalfaJava {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        boolean ulangi = true;
+        
+        while(ulangi){
+            System.out.println("Masukan nilai pertama\t: ");
+            int nilai1 = input.nextInt();
+            System.out.println("Masukan nilai kedua\t: ");
+            int nilai2 = input.nextInt();
+            
+            if(nilai1 > nilai2) {
+                System.out.println(nilai1+ "Lebih besar dari" +nilai2);
+            } else if(nilai1 < nilai2) {
+                System.out.println(nilai1+"Lebih kecil dari" +nilai2);
+            } else {
+                System.out.println(nilai1+"sama dengan" +nilai2);
+            }
+            
+            System.out.println("ulangi aktifitas? (ya/tidak)");
+            String aktifitas = input.next();
+            if(aktifitas.equals("ya")) {
+                System.out.println("Program akan diulangi");
+            } else if (aktifitas.equals("tidak")) {
+                System.out.println("Program berhasil Terimakasih");
+                ulangi = false;
+            }
+        }
+    }
+}
